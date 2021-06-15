86c098a10ab0e65e1395425339b2a224ab77b201
akashvadnala <akashvadnala123@gmail.com>
2021-06-15 16:02:39 +0530

    another changes

--- blog/views.py
+++ blog/views.py
@@ -339,7 +339,7 @@ def prof_update(request):
                 img = request.FILES["image"]
                 data.profile_pic = img
                 data.save()
-    return HttpResponseRedirect(reverse('https://lincart.herokuapp.com/settings'))
+    return HttpResponseRedirect(reverse("https://lincart.herokuapp.com/settings"))
 
 def change_password(request):
     check = register_table.objects.filter(user__id=request.user.id)
