52e869f46001fabc8cd5129bc9c70eb6b13fcac0
akashvadnala <akashvadnala123@gmail.com>
2021-06-16 13:12:20 +0530

    another changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'akashvadnala123',
+        'USER':'akash',
+        'PASSWORD':'1323',
+        'HOST':'lincart.herokuapp.com',
     }
 }
 
