9c2cac76fe5270eb8d4dc15747647369be8c6a06
akashvadnala <akashvadnala123@gmail.com>
2021-06-16 14:33:01 +0530

    another changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -88,7 +88,7 @@ DATABASES = {
         'HOST':'localhost',
     }
 }
-
+'''
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
