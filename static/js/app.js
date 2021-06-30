9a7684f03f536b7ea2d80165d1acdb818dd0fa40
akashvadnala <akashvadnala123@gmail.com>
2021-06-30 17:03:34 +0530

    another changes

--- static/js/app.js
+++ static/js/app.js
@@ -299,7 +299,8 @@ $(document).ready(function () {
 
 //    let socket = new WebSocket(`ws://127.0.0.1:8000/?session_key=${sessionKey}`);
     var socket = new WebSocket(
-        'wss://lincart.herokuapp.com/notifications/ws?session_key=${sessionKey}')
+        'wss://' + window.location.host +
+        '/notifications/ws?session_key=${sessionKey}')
 
     chatInput.keypress(function (e) {
         if (e.keyCode == 13)
